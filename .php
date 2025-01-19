4ae9a0d930dedde1b72cdc14b3c65691ae04eaa5
sidyakina-lunapi <agafoncevvsevolod@gmail.com>
2025-01-20 01:17:36 +0300

    l

---
+++ .php
@@ -0,0 +1,9 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$comment = $_POST['comment'];
+$to = 'Dragom.17school@mail.ru';
+$header = 'Отзыв от пользователя'
+$message = "Имя пользователя: $name \nЭлектронная почта: $email \nОтзыв: $comment";
+$send = mail($to, $header, $message, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
+?>
